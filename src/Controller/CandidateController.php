40b05de2afffad432136ad8497ccf43258df0e58
hamza karfa <hamza.karfa@gmail.com>
2022-02-22 17:04:20 +0100

    add file upload //TODO finish to hydrate candidate with file path

--- src/Controller/CandidateController.php
+++ src/Controller/CandidateController.php
@@ -7,9 +7,12 @@ use App\Form\CandidateType;
 use App\Repository\CandidateRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 #[Route('/candidate')]
 class CandidateController extends AbstractController
@@ -51,12 +54,39 @@ class CandidateController extends AbstractController
     }
 
     #[Route('/{id}/edit', name: 'candidate_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Candidate $candidate, EntityManagerInterface $entityManager): Response
+    public function edit(Request $request, Candidate $candidate, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
     {
         $form = $this->createForm(CandidateType::class, $candidate);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+
+            $this->uploadFile(
+                
+                
+                $form->get("profil_picture")->getData(),
+                $slugger, 
+                'profil_picture_directory' 
+            );
+            $this->uploadFile(
+                
+                
+                $form->get("passport_file")->getData(),
+                $slugger, 
+                'passport_file_directory' 
+            );
+            $this->uploadFile(
+                
+                
+                $form->get("curriculum_vitae")->getData(),
+                $slugger, 
+                'curriculum_vitae_directory' 
+            );
+            dd($candidate);
+            
+
+
+
             $entityManager->flush();
 
             return $this->redirectToRoute('candidate_index', [], Response::HTTP_SEE_OTHER);
@@ -77,5 +107,29 @@ class CandidateController extends AbstractController
         }
 
         return $this->redirectToRoute('candidate_index', [], Response::HTTP_SEE_OTHER);
+    }
+
+
+    public function uploadFile(UploadedFile $brochureFile, SluggerInterface $slugger, string $directory){
+        if ($brochureFile) {
+            $originalFilename = pathinfo($brochureFile->getClientOriginalName(), PATHINFO_FILENAME);
+            // this is needed to safely include the file name as part of the URL
+            $safeFilename = $slugger->slug($originalFilename);
+            $newFilename = $safeFilename.'-'.uniqid().'.'.$brochureFile->guessExtension();
+
+            // Move the file to the directory where brochures are stored
+            try {
+                $brochureFile->move(
+                    $this->getParameter($directory),
+                    $newFilename
+                );
+            } catch (FileException $e) {
+                // ... handle exception if something happens during file upload
+            }
+
+            // updates the 'brochureFilename' property to store the PDF file name
+            // instead of its contents
+            // $product->setBrochureFilename($newFilename);
+        }
     }
 }
--- src/Form/CandidateType.php
+++ src/Form/CandidateType.php
@@ -4,11 +4,14 @@ namespace App\Form;
 
 use App\Entity\Candidate;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
 
 class CandidateType extends AbstractType
 {
@@ -19,27 +22,105 @@ class CandidateType extends AbstractType
                 'choices' =>[
                     'Male'=>'Male',
                     'Female'=>'Female',
-                ]
+                ],
+                'required' => false
             ])
-            ->add('firstname', TextType::class)
-            ->add('lastname', TextType::class)
-            ->add('current_location', TextType::class)
-            ->add('address', TextType::class)
-            ->add('country', TextType::class)
-            ->add('nationality', TextType::class)
-            ->add('birthdate', TextType::class)
-            ->add('birthplace')
-            ->add('passport')
-            ->add('passport_file')
-            ->add('curriculum_vitae')
-            ->add('profil_picture')
-            ->add('availability')
-            ->add('short_description', TextareaType::class)
+            ->add('firstname', TextType::class,[
+                "required"=>false
+            ])
+            ->add('lastname', TextType::class,[
+                "required"=>false
+            ])
+            ->add('current_location', TextType::class,[
+                "required"=>false
+            ])
+            ->add('address', TextType::class,[
+                "required"=>false
+            ])
+            ->add('country', TextType::class,[
+                "required"=>false
+            ])
+            ->add('nationality', TextType::class,[
+                "required"=>false
+            ])
+            ->add('birthdate', TextType::class,[
+                "required"=>false,
+                "mapped"=>false
+            ])
+            ->add('birthplace', TextType::class,[
+                "required"=>false
+            ])
+            ->add('profil_picture', FileType::class,[
+                "required"=>false,
+                "mapped"=>false,
+                "constraints" => [
+                    new File([
+                        'maxSize' => '2000k',
+                        'mimeTypes' => [
+                            'application/pdf',
+                            'application/jpg',
+                            'application/msword',
+                            'image/gif',
+                            'image/jpeg',
+                            'image/png',
+                            'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid PDF document',
+                    ])
+                ],
+            ])
+            ->add('passport', CheckboxType::class,[
+                "required"=>false
+            ])
+            ->add('passport_file', FileType::class,[
+                "required"=>false,
+                "mapped"=>false,
+                "constraints" => [
+                    new File([
+                        'maxSize' => '2000k',
+                        'mimeTypes' => [
+                            'application/pdf',
+                            'application/jpg',
+                            'application/msword',
+                            'image/gif',
+                            'image/jpeg',
+                            'image/png',
+                            'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid PDF document',
+                    ])
+                ],
+            ])
+            ->add('curriculum_vitae', FileType::class,[
+                "required"=>false,
+                "mapped"=>false,
+                "constraints" => [
+                    new File([
+                        'maxSize' => '2000k',
+                        'mimeTypes' => [
+                            'application/pdf',
+                            'application/jpg',
+                            'application/msword',
+                            'image/gif',
+                            'image/jpeg',
+                            'image/png',
+                            'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid PDF document',
+                    ])
+                ],
+            ])
+            // ->add('availability',[
+            //     "required"=>false
+            // ])
+            ->add('short_description', TextareaType::class,[
+                "required"=>false
+            ])
             // ->add('created_at')
             // ->add('updated_at')
             // ->add('user')
-            ->add('job_category')
-            ->add('experience')
+            // ->add('job_category')
+            // ->add('experience')
         ;
     }
 
